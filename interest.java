14fbccf3d9049e0b9680879aed4a12a30606f8b2
Ramya0611 <59659254+Ramya0611@users.noreply.github.com>
2020-02-20 22:10:10 +0530

    Add files via upload

---
+++ interest.java
@@ -0,0 +1,20 @@
+package javaapplication17;
+import java.util.Scanner;
+public class Interest 
+{
+    public static void main (String arg[ ])
+{
+    double pr, rate, t, si,ci;
+    Scanner sc=new Scanner (System. in);
+    System.out.println("Enter the amount = ");
+    pr=sc.nextDouble();
+    System. out. println("Enter the No.of years = ");
+    t=sc.nextDouble();
+    System. out. println("Enter the Rate of  interest = ");
+    rate=sc.nextDouble();
+    si=(pr * t * rate)/100;
+    ci=pr * Math.pow(1.0+rate/100.0,t) - pr;
+    System.out.println("Simple Interest="+si);
+    System.out. println("Compound Interest="+ci);
+   }
+}
